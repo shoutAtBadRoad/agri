3040ef184dba0b81299b9be1a7df2f8ddf595b32
shoutAtBadRoad <79404434+shoutAtBadRoad@users.noreply.github.com>
2022-08-29 16:03:26 +0800

    aop测试时间

---
+++ agri-security/src/main/java/com/agri/aop/TimeAspect.java
@@ -0,0 +1,28 @@
+package com.agri.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+//
+@Component
+@Aspect
+public class TimeAspect {
+
+    @Pointcut("execution(* com.agri.controller.AuthController.*(..))")
+    public void authPointcut() {}
+
+    @Around(value = "authPointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object o = joinPoint.proceed();
+        long end = System.currentTimeMillis();
+        System.out.println(end-start + "ms");
+        return o;
+    }
+}
