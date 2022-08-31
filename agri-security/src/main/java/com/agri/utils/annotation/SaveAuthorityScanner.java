0f817c0d75882b30bf6e7d3dd3c05340d05a0fab
shoutAtBadRoad <79404434+shoutAtBadRoad@users.noreply.github.com>
2022-08-31 15:53:03 +0800

    加载接口权限和角色信息到数据库

--- agri-security/src/main/java/com/agri/utils/annotation/SaveAuthorityScanner.java
+++ agri-security/src/main/java/com/agri/utils/annotation/SaveAuthorityScanner.java
@@ -1,6 +1,16 @@
 package com.agri.utils.annotation;
 
+import com.agri.mapper.SysPermMapper;
+import com.agri.mapper.SysRoleMapper;
+import com.agri.model.SysPerm;
+import com.agri.model.SysRole;
+import com.agri.model.SysRolePerm;
+import com.agri.service.ISysPermService;
+import com.agri.service.ISysRolePermService;
+import com.agri.service.ISysRoleService;
 import com.agri.utils.RedisUtil;
+import com.alibaba.nacos.shaded.org.checkerframework.checker.units.qual.A;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.log4j.Log4j;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
@@ -13,7 +23,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Log4j
 public class SaveAuthorityScanner  implements BeanPostProcessor {
@@ -21,6 +33,14 @@ public class SaveAuthorityScanner  implements BeanPostProcessor {
     @Autowired
     @Qualifier("redisUtil")
     private RedisUtil redisUtil;
+
+    @Autowired
+    private ISysRolePermService rolePermService;
+
+    @Autowired
+    private SysRoleMapper roleMapper;
+    @Autowired
+    private SysPermMapper permMapper;
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
@@ -31,7 +51,9 @@ public class SaveAuthorityScanner  implements BeanPostProcessor {
             return bean;
         String prefix = annotation.value()[0];
         Method[] methods = targetClass.getDeclaredMethods();
+
         for(Method m : methods) {
+            List<Long> roleIds = new ArrayList<>();
             SaveAuth saveAuth = m.getAnnotation(SaveAuth.class);
             PostMapping annotation1 = m.getAnnotation(PostMapping.class);
             if(saveAuth == null || annotation1 == null) {
@@ -41,8 +63,34 @@ public class SaveAuthorityScanner  implements BeanPostProcessor {
             String postfix = annotation1.value()[0];
             String url = prefix + postfix;
             log.info("url : " + url + "\n" + "auths:" + Arrays.toString(auths));
-            redisUtil.lSet(url, CollectionUtils.arrayToList(auths));
-        }
+//            redisUtil.lSet(url, CollectionUtils.arrayToList(auths));
+            // 先存入角色信息
+//            if(url.equals("/sysUser/revisePass")) {
+                for (String auth : auths) {
+                    SysRole role = roleMapper.selectOne(new QueryWrapper<SysRole>().eq("role_key", auth));
+                    if (role == null) {
+                        role = new SysRole();
+                        role.setName(auth);
+                        role.setRoleKey(auth);
+                        roleMapper.insert(role);
+                    }
+                    roleIds.add(role.getId());
+                }
+                // 存入权限信息
+                SysPerm perm = new SysPerm();
+                perm.setPermName("先没名字");
+                perm.setPath(url);
+                permMapper.insert(perm);
+                Long permId = perm.getId();
+                // 存入角色权限关系
+                SysRolePerm sysRolePerm = new SysRolePerm();
+                sysRolePerm.setPermId(permId);
+                for (Long roleId : roleIds) {
+                    sysRolePerm.setRoleId(roleId);
+                    rolePermService.save(sysRolePerm);
+                }
+            }
+//        }
         return bean;
     }
 }
